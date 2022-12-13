1ee2dca4acc8fadb7dbed926d96b4b21f11c3f5b
Mohammad-Sawalha98 <115452622+Mohammad-Sawalha98@users.noreply.github.com>
2022-12-12 16:20:23 -0800

    task1

---
+++ 12-Dec/SQLQuery3.sql
@@ -0,0 +1,31 @@
+create table customers
+(
+id int ,
+firstname varchar,
+lastname varchar,
+);
+
+create table orders
+(
+ordernum int ,
+orderPrice decimal(8,6),
+orderDate varchar,
+);
+
+
+create table products
+(
+bags varchar ,
+balls varchar,
+shirts varchar,
+);
+
+create table employee
+(
+id int ,
+firstname varchar,
+lastname varchar,
+wage int,
+age int,
+position varchar,
+);
